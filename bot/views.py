007e8361d1a22b6cbd9d5df10ff99cff5f44ec26
STQT <productgsm@gmail.com>
2024-02-03 00:53:07 +0500

    fix

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 
 from django.http import HttpResponse
@@ -6,18 +5,11 @@ from django.http import HttpResponse
 from .misc import dp, bot
 
 
-def process_update(request, token: str):
+async def process_update(request, token: str):
     if token == bot.token:
         body_unicode = request.body.decode('utf-8')
         update = json.loads(body_unicode)
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        # Run the coroutine in the event loop
-        loop.run_until_complete(dp.feed_raw_update(bot, update))
-
-        # Close the event loop
-        loop.close()
+        await dp.feed_raw_update(bot, update)
         return HttpResponse(status=200)
     return HttpResponse(status=400)
 
