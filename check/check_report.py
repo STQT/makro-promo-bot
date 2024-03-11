0a07f80af0ccaa65dfec6ef270cc15aed6ad06d0
STQT <productgsm@gmail.com>
2024-03-11 17:33:18 +0500

    fix checking from the api

--- check/check_report.py
+++ check/check_report.py
@@ -1,22 +1,37 @@
-from openpyxl import load_workbook
+import requests
+from openpyxl import Workbook
+from openpyxl.reader.excel import load_workbook
 
 # Load the promo.xlsx file
 promo_workbook = load_workbook('promo.xlsx')
 promo_sheet = promo_workbook.active
 
-# Load the bot2.xlsx file
-bot_workbook = load_workbook('bot.xlsx')
-bot_sheet = bot_workbook.active
+# Create a new workbook and worksheet
+output_workbook = Workbook()
+output_sheet = output_workbook.active
 
-# Iterate over the rows in bot2.xlsx
-for i, bot_row in enumerate(bot_sheet.iter_rows(min_row=2, values_only=True), start=2):
-    bot_value = str(bot_row[3])  # Assuming the value is in the third column
-    for promo_row in promo_sheet.iter_rows(min_row=2, values_only=True):
-        promo_value = str(promo_row[0])
-        exists = bot_value == promo_value
-        result = 'Существует' if exists else 'Не существует'
-        # Write the result to the fifth column
-        bot_sheet.cell(row=i, column=6, value=result)
+# Add headers to the output sheet
+output_sheet.append(["Phone", "Fullname", "Promotion", "Code", "Created At"])
+token = ""
 
-# Save the changes to bot2.xlsx
-bot_workbook.save('final.xlsx')
+for num, promo_row in enumerate(promo_sheet.iter_rows(min_row=2, values_only=True)):
+    promo_value = str(promo_row[0])
+    api_url = f'https://bot.makromarket.uz/promo/{promo_value}/'
+    headers = {'Authorization': f'Token {token}'}
+    response = requests.get(api_url, headers=headers)
+    print(response)
+    if response.status_code == 200:
+        data = response.json()
+        user = data["user"]
+        if "phone" in user and "fullname" in user:
+            output_sheet.append([
+                user["phone"],
+                user["fullname"],
+                data["promotion"],
+                data["code"],
+                data["created_at"]
+            ])
+    print(num)
+
+# Save the output workbook
+output_workbook.save("output.xlsx")
